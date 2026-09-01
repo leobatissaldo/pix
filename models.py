75e4f3b73a238e09e54d94144d12ca207a0296ff
leobatissaldo <leobatissaldo@gmail.com>
2026-09-01 15:55:50 -0300

    adds delete account at service and account status at model

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey, Boolean
 
 class Conta(Base):
     __tablename__ = "conta"
@@ -7,6 +7,7 @@ class Conta(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome_titular = Column(String, nullable=False)
     saldo = Column(Float, nullable=False)
+    conta_ativa = Column(Boolean, nullable=False)
 
 class Transação(Base):
     __table__ = "transacao"
--- service.py
+++ service.py
@@ -1,15 +1,31 @@
 from database import get_db
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from models import Conta
+from schemas import ContaInput
 
-class Transacao_Service():
+class Conta_Service():
     def __init__(self, db: Session):
         self.db = db
 
-    def criar_conta(self):
+    def criar_conta(self, input: ContaInput):
         novo_usuario = Conta(nome_titular=input.nome, saldo=input.saldo)
         self.db.add(novo_usuario)
         self.db.commit()
         self.db.refresh(novo_usuario)
         return novo_usuario
+
+    def deletar_conta(self, id: int):
+        conta_deletada = self.db.scalars(select(Conta).where(id == Conta.id)).first()
+        if conta_deletada.conta_ativa == False:
+            return {"message": "essa conta já esta desativada!"}
+        elif conta_deletada.conta_ativa == True:
+            conta_deletada.conta_ativa == False
+            return {"message": "conta desativada com sucesso!"}
+        else:
+            raise HTTPException(status_code=404, detail="Conta não encontrada")
+
+class Transacao_Service():
+    def __init__(self, db: Session):
+        self.db = db
