dc21d5b30d3269e6105b2a5152b6a6774db3e51c
leobatissaldo <leobatissaldo@gmail.com>
2026-09-01 14:03:06 -0300

    feat: add postgree setup

--- database.py
+++ database.py
@@ -2,6 +2,18 @@ from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postgresql://postgres:leo@localhost:5432/mock_pix"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 class Base(DeclarativeBase):
     pass
 
