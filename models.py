17e9262979aeb861fbd96b6bff8c4f74f25d7760
leobatissaldo <leobatissaldo@gmail.com>
2026-09-01 14:54:56 -0300

    feat: add exchange model

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
 
 class Conta(Base):
     __tablename__ = "conta"
@@ -7,3 +7,13 @@ class Conta(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome_titular = Column(String, nullable=False)
     saldo = Column(Float, nullable=False)
+
+class Transação(Base):
+    __table__ = "transacao"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    horario = Column(DateTime, nullable=False)
+    valor = Column(Integer, nullable=False)
+    conta_partida_id = Column(Integer, ForeignKey("conta.id"))
+    conta_destino_id = Column(Integer, ForeignKey("conta.id"))
+    
